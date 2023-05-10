fbf977c3cd250f0bfd69211db95f34036eba5e31
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-10 17:21:46 +0400

    EF

---
+++ CS/EFCore/GenerateRoleEF/GenerateRoleEF.Module/BusinessObjects/GenerateRoleEFDbContext.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace GenerateRoleEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class GenerateRoleEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<GenerateRoleEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new GenerateRoleEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class GenerateRoleEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<GenerateRoleEFEFCoreDbContext> {
+	public GenerateRoleEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<GenerateRoleEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=GenerateRoleEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new GenerateRoleEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(GenerateRoleEFContextInitializer))]
+public class GenerateRoleEFEFCoreDbContext : DbContext {
+	public GenerateRoleEFEFCoreDbContext(DbContextOptions<GenerateRoleEFEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<GenerateRoleEF.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<GenerateRoleEF.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<GenerateRoleEF.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ CS/EFCore/GenerateRoleEF/GenerateRoleEF.Win/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win.ApplicationBuilder;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.XtraEditors;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.ExpressApp.Design;
+
+namespace GenerateRoleEF.Win;
+
+public class ApplicationBuilder : IDesignTimeApplicationFactory {
+    public static WinApplication BuildApplication(string connectionString) {
+        var builder = WinApplication.CreateBuilder();
+        builder.UseApplication<GenerateRoleEFWindowsFormsApplication>();
+        builder.Modules
+            .AddConditionalAppearance()
+            .AddValidation(options => {
+                options.AllowValidationDetailsAccess = false;
+            })
+            .Add<GenerateRoleEF.Module.GenerateRoleEFModule>()
+        	.Add<GenerateRoleEFWinModule>();
+        builder.ObjectSpaceProviders
+            .AddSecuredEFCore().WithDbContext<GenerateRoleEF.Module.BusinessObjects.GenerateRoleEFEFCoreDbContext>((application, options) => {
+                // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
+                // Do not use this code in production environment to avoid data loss.
+                // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
+                //options.UseInMemoryDatabase("InMemory");
+                options.UseSqlServer(connectionString);
+                options.UseChangeTrackingProxies();
+                options.UseObjectSpaceLinkProxies();
+            })
+            .AddNonPersistent();
+        builder.Security
+            .UseIntegratedMode(options => {
+                options.RoleType = typeof(PermissionPolicyRole);
+                options.UserType = typeof(GenerateRoleEF.Module.BusinessObjects.ApplicationUser);
+                options.UserLoginInfoType = typeof(GenerateRoleEF.Module.BusinessObjects.ApplicationUserLoginInfo);
+            })
+            .UsePasswordAuthentication();
+        builder.AddBuildStep(application => {
+            application.ConnectionString = connectionString;
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        });
+        var winApplication = builder.Build();
+        return winApplication;
+    }
+
+    XafApplication IDesignTimeApplicationFactory.Create()
+        => BuildApplication(XafApplication.DesignTimeConnectionString);
+}
