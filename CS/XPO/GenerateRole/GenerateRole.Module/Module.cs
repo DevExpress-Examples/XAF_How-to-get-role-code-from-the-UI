1ebd79badda1c69c3101a280bfe6c8b0ecbd2dcb
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-10 16:23:51 +0400

    init xpo

---
+++ CS/XPO/GenerateRole/GenerateRole.Module/Module.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+
+namespace GenerateRole.Module;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
+public sealed class GenerateRoleModule : ModuleBase {
+    public GenerateRoleModule() {
+		// 
+		// GenerateRoleModule
+		// 
+		AdditionalExportedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifference));
+		AdditionalExportedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifferenceAspect));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Security.SecurityModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.ValidationModule));
+    }
+    public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        return new ModuleUpdater[] { updater };
+    }
+    public override void Setup(XafApplication application) {
+        base.Setup(application);
+        // Manage various aspects of the application UI and behavior at the module level.
+    }
+    public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
+        base.CustomizeTypesInfo(typesInfo);
+        CalculatedPersistentAliasHelper.CustomizeTypesInfo(typesInfo);
+    }
+}
