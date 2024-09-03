23af8aa0a0f206be00147f6456fcb4be00a8dba6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2024-09-03 15:47:44 +0400

    Create 24.2.1+ branch

--- CS/EFCore/GenerateRoleEF/GenerateRoleEF.Blazor.Server/Startup.cs
+++ CS/EFCore/GenerateRoleEF/GenerateRoleEF.Blazor.Server/Startup.cs
@@ -56,6 +56,7 @@ public class Startup {
                     options.UseSqlServer(connectionString);
                     options.UseChangeTrackingProxies();
                     options.UseObjectSpaceLinkProxies();
+                    options.UseXafServiceProviderContainer(serviceProvider);
                     options.UseLazyLoadingProxies();
                 })
                 .AddNonPersistent();
