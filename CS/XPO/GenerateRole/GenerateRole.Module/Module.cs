11a1ab712eddd3f3d9d2820ff1a319d38ad1e2f1
Andrey K <andrey.kozhevnikov@devexpress.com>
2025-11-26 12:25:33 +0400

    remove objects

--- CS/XPO/GenerateRole/GenerateRole.Module/Module.cs
+++ CS/XPO/GenerateRole/GenerateRole.Module/Module.cs
@@ -26,7 +26,6 @@ public sealed class GenerateRoleModule : ModuleBase {
 		AdditionalExportedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifferenceAspect));
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Security.SecurityModule));
-		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule));
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.ValidationModule));
     }
